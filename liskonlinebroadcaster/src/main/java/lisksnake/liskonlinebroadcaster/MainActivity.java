d37b28e38aea2a70b7569fd86d28e95c2298cdc0
Thomas L <lisksnake@gmx.de>
2018-02-04 21:19:41 +0100

    Fixed issue with custom node

--- liskonlinebroadcaster/src/main/java/lisksnake/liskonlinebroadcaster/MainActivity.java
+++ liskonlinebroadcaster/src/main/java/lisksnake/liskonlinebroadcaster/MainActivity.java
@@ -247,7 +247,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case 3:         // CUSTOM NODE selected
                     strNetHash = "ed14889723f24ecc54871d058d98ce91ff2f973192075c0155ba2b7b70ad2511";
-                    strURL = textCustomNode.toString();
+                    strURL = String.valueOf(textCustomNode.getText()) + "/peer/transactions";
                     break;
                 default:
                     strNetHash = "da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba";
